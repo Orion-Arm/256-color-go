f7a236def1d91b385f7ca9d3b5789cec28a611cc
A-Polyglot <92436209+a-polyglot@users.noreply.github.com>
2022-11-02 16:16:13 +0530

    Update main.go

--- main.go
+++ main.go
@@ -16,15 +16,14 @@ func setConsoleColors() error {
 }
 
 func Color(colorcode int) string {
-	setConsoleColors()
+	setConsoleColors() // init of terminal
 	code := strconv.Itoa(colorcode)
-	color := "\u001b[38;5;" + code + "m"
-	fmt.Print("\u001b[0m")
+	color := "\u001b[38;5;" + code + "m" // acii code used by terminals to print colors
 	return fmt.Sprintf(color)
 
 }
 
 func main() {
-	fmt.Printf("%vThis is color\n", Color(140))
-	fmt.Print("\u001b[0m")
+	fmt.Printf("%vThis is color\n", Color(140)) // color code 140
+	fmt.Print("\u001b[0m") // This is to reset the terminal color 
 }
